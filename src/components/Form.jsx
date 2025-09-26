518610fb472380618cb53207905b77224cbe0c45
khalid <student3045612@gmail.com>
2025-09-26 15:40:10 +0530

    add search  box

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,30 +5,32 @@ function Form({ handleFormSubmitData }) {
 
   return (
     <div className="my-10 flex justify-center ">
-      <form className="flex items-center gap-10 flex-wrap justify-start" onSubmit={handleSubmit(data => handleFormSubmitData(data))}>
+      <form
+        className="flex items-center gap-10 flex-wrap justify-center "
+        onSubmit={handleSubmit((data) => handleFormSubmitData(data))}
+      >
         <input
-          className=" rounded-xl py-5 px-4 text-base font-semibold outline-none border "
+          className=" rounded-xl py-5 px-4 text-base font-semibold outline-none border bg-white opacity-60"
           type="text"
           placeholder="Song name"
-          {...register('name')}
+          {...register("name")}
         />
         <input
-          className="rounded-xl py-5 px-4 text-base font-semibold outline-none border"
+          className="rounded-xl py-5 px-4 text-base font-semibold outline-none border bg-white opacity-60"
           type="text"
           placeholder="singer"
-          {...register('singer')}
+          {...register("singer")}
         />
         <input
-          className="rounded-xl py-5 px-4 text-base font-semibold outline-none border"
+          className="rounded-xl py-5 px-4 text-base font-semibold outline-none border bg-white opacity-60"
           type="text"
           placeholder="image url"
-          {...register('image')}
+          {...register("image")}
         />
-        <input
-          type="submit"
-          value="Submit"
-          className="bg-violet-600 text-sm font-semibold text-white px-10 py-5 rounded-2xl border border-transparent hover:bg-violet-400 hover:border-purple-600 hover:scale-95 transition-all duration-300 "
-        />
+
+        <button className="bg-violet-600 text-sm  font-semibold text-white px-10 py-5 rounded-2xl border border-transparent hover:bg-violet-400 hover:border-purple-600 hover:scale-95 transition-all duration-300 ">
+          Submit
+        </button>
       </form>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,41 @@
-import React from "react";
-import styles from "./style.module.css" // import styles from custom css
+
+import styles from "./style.module.css"; // import styles from custom css
 // music card navbar
 function Navbar({ musicData }) {
-    return (
-        <div className="w-full px-5 sm:px-10 lg:px-20 py-3 flex justify-between items-center border-b-2 border-b-orange-500">
-            <h1 className="lg:text-4xl md:text-2xl sm:text-xl text-lg  font-bold text-violet-300">Favourite Songs</h1>
-            <div className="flex p-2 px-4 bg-violet-600 rounded-lg text-white text-sm gap-3 ">
-                     {/* adding custom css classes  */}
-                    <h3 className={styles.fonts}>Favourites</h3>
-                    {/* adding multiple custom css classes */}
-                    <h4 className={`${styles.fonts} ${styles.favouritesCount}`}>{musicData.filter(item => item.added).length}</h4>
-                </div >
-            </div>
-        
-    );
+  
+  return (
+    <div className="w-full px-5 sm:px-10 lg:px-20 py-3  flex-wrap  items-center border-b-2 border-b-purple-400 flex justify-between gap-y-2">
+      <h1 className="lg:text-4xl md:text-2xl sm:text-xl text-md  font-bold text-violet-300 ">
+        Favourite Songs
+      </h1>
+      {/* search */}
+
+      <input
+        type="search"
+        name="search"
+        id="search"
+        placeholder="Search Your Favourite"
+        className="rounded-2xl px-3 py-4 border border-purple-400 text-white font-bold text-sm w-auto   transition-all duration-500 ease-in-out focus-within:bg-white focus-within:text-purple-700 md:block hidden"
+      />
+
+      <div className="flex p-2 px-4 bg-violet-600 rounded-lg text-white text-sm gap-3 ">
+        {/* adding custom css classes  */}
+        <h3 className={styles.fonts}>Favourites</h3>
+        {/* adding multiple custom css classes */}
+        <h4 className={`${styles.fonts} ${styles.favouritesCount}`}>
+          {musicData.filter((item) => item.added).length}
+        </h4>
+      </div>
+
+      <input
+        type="search"
+        name="search"
+        id="search"
+        placeholder="Search Your Favourite"
+        className="rounded-2xl px-3 py-4 border border-purple-400 text-white font-bold text-sm w-auto   transition-all duration-500 ease-in-out focus-within:bg-white focus-within:text-purple-700 md:hidden basis-full block my-4"
+      />
+    </div>
+  );
 }
 
 export default Navbar;
-
-   
